4e8df5042c241bdcbb57b7babe607c5386ffcfca
2118089226@qq.com <2118089226@qq.com>
2019-06-17 21:10:34 +0800

    初始化

--- zuul-server/src/main/java/com/data4truth/pi/Application.java
+++ zuul-server/src/main/java/com/data4truth/pi/Application.java
@@ -2,6 +2,7 @@ package com.data4truth.pi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableZuulProxy
+@RefreshScope
 public class Application {
 
     public static void main( String[] args ) {
